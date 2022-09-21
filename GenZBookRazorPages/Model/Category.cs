ef957e345d46653e759d13ccdfeb31216d0ef865
myntd1902 <myntd1902@gmail.com>
2022-09-21 14:09:48 +0700

    first commit

---
+++ GenZBookRazorPages/Model/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace GenZBookRazorPages.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
